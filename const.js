a8531655fce220da2f90b7fbb9f62157e45fe232
Aleksandr232 <aleksrowing232@gmail.com>
2022-09-03 17:19:03 +0300

    site

--- const.js
+++ const.js
@@ -1,6 +1,7 @@
 const commands = `
 /start - Запустить бота 
 /help - Помощь
+/site - Наш сайт
 `
 
 module.exports.commands = commands
--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) =>ctx.reply(`Привет, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'друг'}, я помогу тебе с высотными работами`));
 bot.hears('Привет', (ctx) => ctx.reply('Привет, помогу с высотными работами'));
 bot.help((ctx)=>ctx.reply(commBot.commands))
+
+bot.command('site', (ctx)=>{
+    ctx.replyWithHTML('<a href="http://on-high.ru/">Наш сайт</a>')
+})
+
 bot.launch();
 
 // Enable graceful stop
